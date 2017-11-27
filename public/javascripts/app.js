f4ac21a3dedee566062284550b75f67884f2d145
Stanley <stanley.e.varner@gmail.com>
2017-11-27 21:43:44 +0000

    first commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,49 @@
+angular.module('comment', [])
+.controller('MainCtrl', [
+	'$scope',
+  '$http',
+	function($scope, $http){
+		$scope.comments = [];
+
+		$scope.addComment = function() {
+			//$scope.comments.push({title:$scope.formContent,upvotes:0});
+      if($scope.formContent === '') { return; }
+      console.log("In addComment with "+$scope.formContent);
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0,
+      });
+      $scope.formContent = '';
+		};
+
+    $scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
+		};
+    $scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+        });
+    };
+    $scope.getAll = function() {
+  return $http.get('/comments').success(function(data){
+    angular.copy(data, $scope.comments);
+  });
+};
+$scope.getAll();
+$scope.create = function(comment) {
+    return $http.post('/comments', comment).success(function(data){
+      $scope.comments.push(data);
+    });
+  };
+  $scope.delete = function(comment) {
+    $http.delete('/comments/' + comment._id )
+      .success(function(data){
+        console.log("delete worked");
+      });
+    $scope.getAll();
+  };
+	}
+
+]);
